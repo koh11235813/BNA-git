3de37e40c6dbd3ac7f9ea72761eb1808a08ae1f7
koh11235813 <koh11235813@gmail.com>
2024-07-26 17:24:00 +0900

    fix

--- 6/main.c
+++ 6/main.c
@@ -11,7 +11,7 @@ double MSE(double v)
 
 double function(double v)
 {
-    return SIGMA+ALPHA*MSE(v) - v; 
+    return SIGMA+ALPHA*MSE(v) - v;
 }
 
 double analysis(double v)
@@ -40,7 +40,7 @@ void Bisection(double xl0, double xr0)
     }
 }
 
-void mseBisection(double xl0, double xr0)
+double mseBisection(double xl0, double xr0)
 {
     int i, number;
     double x, xl, xr, xs, yl_sign;
@@ -50,7 +50,7 @@ void mseBisection(double xl0, double xr0)
     xr = xr0;
 
     yl_sign = function(xl0);
-    for (i = 1; i <= number && xr - xl > EPS; i++) {
+    for (i = 1; i <= number; i++) {
         x = (xl + xr)/2.0;
         printf("Iteration = %d, solution = %g, error = %g \n", i, x, fabs(function(xl) - function(xr)));
         if(yl_sign*function(x) > 0.0) {
@@ -58,7 +58,11 @@ void mseBisection(double xl0, double xr0)
         }else{
             xr = x;
         }
+        if (xr - xl < EPS){
+            return x;
+        }
     }
+    return -1;
 }
 int main(void)
 {
@@ -70,9 +74,9 @@ int main(void)
 
     printf("xr0:");
     scanf("%lf",&xr0);
-    
+
     puts("\n1.\n");
-    mseBisection(xl0, xr0);
+    mse = mseBisection(xl0, xr0);
 
     puts("\nanalysis\n");
     Bisection(xl0, xr0);
